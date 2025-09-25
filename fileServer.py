96197cd74412d4e502bcaa8ac47c3253d292b879
VaradLanke <lanke.varad@gmail.com>
2025-09-25 21:47:53 +0530

    <Update> Deprecated api replaced

--- fileServer.py
+++ fileServer.py
@@ -1,28 +1,39 @@
 import http.server, ssl, os
 
-# absolute path of pyServer.py
+# absolute path of fileServer.py
 thisScriptPath=os.path.dirname(os.path.abspath(__file__))+'/'
+cert_path = thisScriptPath + 'cert.pem'
+key_path = thisScriptPath + 'key.pem'
 
 # generate self signed certificate using openssl command
 def generate_selfsigned_cert():
     try:
-        OpenSslCommand = 'openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out '+thisScriptPath+'cert.pem -keyout '+thisScriptPath+'key.pem -subj "/C=IN/ST=Maharashtra/L=Satara/O=Wannabees/OU=KahiHiHa Department/CN=www.iamselfdepartment.com"'
-        os.system(OpenSslCommand)
-        print('<<<<Certificate Generated>>>>>>')
+        if os.path.exists(cert_path) and os.path.exists(key_path):
+            print('[WARN]: Certificate already exists.')
+            return
+
+        OpenSslCommand = f'openssl req -newkey rsa:4096 -x509 -sha256 -days 3650 -nodes -out "{cert_path}" -keyout "{key_path}" -subj "/C=IN/ST=Maharashtra/L=Satara/O=Wannabees/OU=KahiHiHa Department/CN=www.iamselfdepartment.com"'
+        ret = os.system(OpenSslCommand)
+
+        if ret == 0 and os.path.exists(cert_path) and os.path.exists(key_path):
+            print('[INFO]: <<<<Certificate Generated>>>>>>')
+        else:
+            print('[ERROR]: Failed to generate certificate. Please Check Whether OpenSsl is installed and added in PATH')
+            exit(1)
     except:
-        print('error while generating certificate')
+        print('[ERROR]: Exiting with error...')
+        exit(1)
 
 # starts server on provided host and port
 def startServer(host,port):
     server_address = (host, port)
     httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-    httpd.socket = ssl.wrap_socket(httpd.socket,
-                                server_side=True,
-                                certfile = thisScriptPath+"cert.pem",
-                                keyfile = thisScriptPath+"key.pem",
-                                ssl_version=ssl.PROTOCOL_TLSv1
-                                )
-    print("File Server started at https://" + server_address[0]+":"+str(server_address[1]))
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile=thisScriptPath + "cert.pem", keyfile=thisScriptPath + "key.pem")
+
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+
+    print("[INFO]: File Server started at https://" + server_address[0] + ":" + str(server_address[1]))
     httpd.serve_forever()
 
 # entry point of script
@@ -32,7 +43,7 @@ def main():
         # you can change the host and port
         startServer('localhost',8000)
     except KeyboardInterrupt:
-        print("\nFile Server Stopped!")
+        print("\n[INFO]: File Server Stopped!")
 
 # call to main function
 main()
